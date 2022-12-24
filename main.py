f8237589009086b4b5c733e4bbfaf12494048695
Abelito Faleyrio Visese <91595663+GaTheLy@users.noreply.github.com>
2022-12-24 22:11:33 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from tkinter import messagebox
+import tkinter
 
 # Set window width and height
 pygame.init()
@@ -7,8 +7,8 @@ window_w = 600
 window_h = 600
 
 SCREEN = pygame.display.set_mode((window_w, window_h))
-pygame.display.set_caption("Main Menu")
-fps = 30
+
+fps = 60
 timer = pygame.time.Clock()
 
 font = pygame.font.Font('freesansbold.ttf', 32)
@@ -36,13 +36,16 @@ cur = None
 queue = []
 stack = []
 
-
-# Load gambar
-BG = pygame.image.load("assets/bg-1.jpg")
+neigh_btn = []
+n_btn_x = []
+n_btn_y = []
+neighbours = [True] * 9
+# print(neighbours)
 
 
 class Box:
     def __init__(self, i, j):
+        # Posisi di grid (grid[x][y])
         self.x = i
         self.y = j
         self.neigh = []
@@ -57,19 +60,38 @@ class Box:
     def draw(self, win, color):
         self.box = pygame.draw.rect(win, color, (self.x * box_w, self.y * box_h, box_w-2, box_h-2))
 
+    def reset_neighbours(self):
+        self.neigh = []
+
     def set_neighbours(self):
-        # Atas bawah kanan kiri
-        if self.x > 0:
-            self.neigh.append(grid[self.x - 1][self.y])
-        if self.x < col - 1:
-            self.neigh.append(grid[self.x + 1][self.y])
-        if self.y > 0:
+        # Kiri atas
+        if (self.x > 0 and self.y > 0) and neighbours[0]:
+            self.neigh.append(grid[self.x - 1][self.y - 1])
+        # Atas
+        if self.y > 0 and neighbours[3]:
             self.neigh.append(grid[self.x][self.y - 1])
-        if self.y < row - 1:
+        # Kanan atas
+        if (self.x < col - 1 and self.y > 0) and neighbours[6]:
+            self.neigh.append(grid[self.x + 1][self.y - 1])
+        # Kiri
+        if self.x > 0 and neighbours[1]:
+            self.neigh.append(grid[self.x - 1][self.y])
+        # Kanan
+        if self.x < col - 1 and neighbours[7]:
+            self.neigh.append(grid[self.x + 1][self.y])
+        # Kanan bawah
+        if (self.x < col - 1 and self.y < row - 1) and neighbours[8]:
+            self.neigh.append(grid[self.x + 1][self.y + 1])
+        # Bawah
+        if self.y < row - 1 and neighbours[5]:
             self.neigh.append(grid[self.x][self.y + 1])
+        # Kiri bawah
+        if (self.x > 0 and self.y < row - 1) and neighbours[2]:
+            self.neigh.append(grid[self.x - 1][self.y+1])
 
 
 def draw_menu():
+    pygame.display.set_caption("Main Menu")
     SCREEN.fill('black')
     # com ini temp variable buat state
     com = 0
@@ -110,9 +132,11 @@ def draw_menu():
     return com
 
 
-def draw_djik(condition, search, queue, cur, nemu):
+def draw_djik(condition, search, queue, cur, nemu, path):
+    pygame.display.set_caption("Dijkstra")
     # if queue:
-    #     print(len(queue))
+    #     for box in queue:
+    #         print(box.x, box.y)
     ready = None
 
     SCREEN.fill('#343434')
@@ -120,6 +144,10 @@ def draw_djik(condition, search, queue, cur, nemu):
     con = condition
 
     check_color()
+
+    font_1 = pygame.font.Font('freesansbold.ttf', 10)
+    dis_n_btn = font_1.render('Set Neighbours', True, 'white')
+    SCREEN.blit(dis_n_btn, (512, 505))
 
     # Dalem
     menu_btn = pygame.draw.rect(SCREEN, 'gray', [225, 525, 150, 50], 0, 5)
@@ -141,19 +169,26 @@ def draw_djik(condition, search, queue, cur, nemu):
     s_text = font_1.render('E', True, 'black')
     SCREEN.blit(s_text, (122, 538))
 
-    w_btn = pygame.draw.rect(SCREEN, 'black', [45, 525, 50, 50], 0, 5)
+    w_col = pygame.Color('black')
+    if con == 4:
+        w_col = pygame.Color('red')
+    w_btn = pygame.draw.rect(SCREEN, w_col, [45, 525, 50, 50], 0, 5)
     pygame.draw.rect(SCREEN, 'dark gray', [45, 525, 50, 50], 5, 5)
     s_text = font_1.render('W', True, 'white')
     SCREEN.blit(s_text, (59, 538))
 
+    # print(len(neigh_btn), len(neighbours), print(neighbours))
+
     if menu_btn.collidepoint(pygame.mouse.get_pos()):
+        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
         menu_text = font_1.render('Main Menu', True, 'beige')
         SCREEN.blit(menu_text, (235, 535))
         if pygame.mouse.get_pressed()[0]:
             # State berubah
             com = 0
 
-    if s_btn.collidepoint(pygame.mouse.get_pos()):
+    elif s_btn.collidepoint(pygame.mouse.get_pos()):
+        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
         s_text = font_1.render('S', True, 'beige')
         SCREEN.blit(s_text, (182, 538))
         if pygame.mouse.get_pressed()[0]:
@@ -161,19 +196,26 @@ def draw_djik(condition, search, queue, cur, nemu):
             con = 1
             # messagebox.showinfo("cek aja", f"{con}")
 
-    if e_btn.collidepoint(pygame.mouse.get_pos()):
+    elif e_btn.collidepoint(pygame.mouse.get_pos()):
+        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
         e_text = font_1.render('E', True, 'beige')
         SCREEN.blit(e_text, (122, 538))
         if pygame.mouse.get_pressed()[0]:
             con = 2
             # messagebox.showinfo("cek aja", f"{con}")
 
-    if w_btn.collidepoint(pygame.mouse.get_pos()):
+    elif w_btn.collidepoint(pygame.mouse.get_pos()):
+        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
         menu_text = font_1.render('W', True, 'beige')
         SCREEN.blit(menu_text, (58, 538))
         if pygame.mouse.get_pressed()[0]:
-            con = 3
+            if con == 3:
+                con = 4
+            else:
+                con = 3
             # messagebox.showinfo("cek aja", f"{con}")
+    else:
+        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
 
     if nemu and not cur.start:
         path.append(cur)
@@ -182,9 +224,29 @@ def draw_djik(condition, search, queue, cur, nemu):
     if not search:
         ready, queue, stack, end_node = check_ready()
 
-    if event.type == pygame.KEYDOWN and ready:
-        search = True
-        # print(queue[0].x, queue[0].y)
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            print(event.type)
+            print(event.key)
+            if event.key == pygame.K_RETURN and ready:
+                path = reset_path_etc()
+                # print("test hemm")
+                assign_neighbours()
+                search = True
+                nemu = False
+            if event.key == pygame.K_w:
+                print("Test W")
+                if con == 3:
+                    con = 4
+                else:
+                    con = 3
+            if event.key == pygame.K_s:
+                con = 1
+            if event.key == pygame.K_e:
+                con = 2
+        if event.type == pygame.QUIT:
+            run[0] = False
+
     if queue and search:
         cur = queue.pop(0)
         cur.visited = True
@@ -207,10 +269,11 @@ def draw_djik(condition, search, queue, cur, nemu):
             pygame.draw.rect(SCREEN, 'black', [200, 200, 200, 200], 0, 5)
             search = False
 
-    return com, con, search, queue, cur, nemu
+    return com, con, search, queue, cur, nemu, path
 
 
-def draw_dfs(condition, search, stack, cur, nemu):
+def draw_dfs(condition, search, stack, cur, nemu, path):
+    pygame.display.set_caption("Depth First Search")
     ready = None
 
     SCREEN.fill('#343434')
@@ -219,6 +282,10 @@ def draw_dfs(condition, search, stack, cur, nemu):
     con = condition
 
     check_color()
+
+    font_1 = pygame.font.Font('freesansbold.ttf', 10)
+    dis_n_btn = font_1.render('Set Neighbours', True, 'white')
+    SCREEN.blit(dis_n_btn, (512, 505))
 
     menu_btn = pygame.draw.rect(SCREEN, 'gray', [225, 525, 150, 50], 0, 5)
     pygame.draw.rect(SCREEN, 'dark gray', [225, 525, 150, 50], 5, 5)
@@ -236,7 +303,10 @@ def draw_dfs(condition, search, stack, cur, nemu):
     s_text = font_1.render('E', True, 'black')
     SCREEN.blit(s_text, (122, 538))
 
-    w_btn = pygame.draw.rect(SCREEN, 'black', [45, 525, 50, 50], 0, 5)
+    w_col = pygame.Color('black')
+    if con == 4:
+        w_col = pygame.Color('red')
+    w_btn = pygame.draw.rect(SCREEN, w_col, [45, 525, 50, 50], 0, 5)
     pygame.draw.rect(SCREEN, 'dark gray', [45, 525, 50, 50], 5, 5)
     s_text = font_1.render('W', True, 'white')
     SCREEN.blit(s_text, (59, 538))
@@ -275,8 +345,28 @@ def draw_dfs(condition, search, stack, cur, nemu):
     if not search:
         ready, queue, stack, end_node = check_ready()
 
-    if event.type == pygame.KEYDOWN and ready:
-        search = True
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            print(event.type)
+            print(event.key)
+            if event.key == pygame.K_RETURN and ready:
+                # print("test hemm")
+                path = reset_path_etc()
+                assign_neighbours()
+                search = True
+                nemu = False
+            if event.key == pygame.K_w:
+                print("Test W")
+                if con == 3:
+                    con = 4
+                else:
+                    con = 3
+            if event.key == pygame.K_s:
+                con = 1
+            if event.key == pygame.K_e:
+                con = 2
+        if event.type == pygame.QUIT:
+            run[0] = False
         # print(queue[0].x, queue[0].y)
     if stack and search:
         cur = stack.pop()
@@ -299,7 +389,7 @@ def draw_dfs(condition, search, stack, cur, nemu):
             pygame.draw.rect(SCREEN, 'black', [200, 200, 200, 200], 0, 5)
             search = False
 
-    return com, con, search, stack, cur, nemu
+    return com, con, search, stack, cur, nemu, path
 
 
 def check_color():
@@ -323,6 +413,15 @@ def check_color():
             if box.end:
                 box.draw(SCREEN, "#FF8800")
 
+    for i in range(len(neigh_btn)):
+        if i == 4:
+            neigh_btn[i].draw(SCREEN, "#82EEFD")
+        else:
+            neigh_btn[i].draw(SCREEN, "#48AAAD")
+
+        if not neighbours[i]:
+            neigh_btn[i].draw(SCREEN, "#FF5C5C")
+
 
 # Mengisi Grid
 def make_grid():
@@ -331,6 +430,10 @@ def make_grid():
         for j in range(row):
             arr.append(Box(i, j))
         grid.append(arr)
+
+    for i in range(26, 29):
+        for j in range(26, 29):
+            neigh_btn.append(Box(i, j))
 
 
 def reset_grid():
@@ -343,8 +446,17 @@ def reset_grid():
             box.visited = False
             box.prior = None
 
-    return [], False
+    return [], False, False, [True] * 9
 
+
+def reset_path_etc():
+    for i in grid:
+        for box in i:
+            box.queued = False
+            box.visited = False
+            box.prior = None
+
+    return []
 
 
 def delete_start():
@@ -359,12 +471,11 @@ def delete_end():
             box.end = False
 
 
-make_grid()
-
-# Mengisi Neighbour
-for i in range(col):
-    for j in range(row):
-        grid[i][j].set_neighbours()
+def assign_neighbours():
+    for i in range(col):
+        for j in range(row):
+            grid[i][j].reset_neighbours()
+            grid[i][j].set_neighbours()
 
 
 def check_ready():
@@ -381,6 +492,7 @@ def check_ready():
             if box.start:
                 cond1 += 1
                 # Isi queue dengan Start Node
+                # box.queued = True
                 q = [box]
                 s = [box]
             if box.end:
@@ -392,44 +504,80 @@ def check_ready():
     return False, q, s, end_n
 
 
-def check_collide(con):
+def check_collide(con, nemu, path, searching):
     for l in grid:
         for box in l:
             if box.box.collidepoint(pygame.mouse.get_pos()):
+                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
                 box.draw(SCREEN, (169, 169, 169))
                 if pygame.mouse.get_pressed()[0]:
-                    if con == 1:
+                    if con == 1 and not box.wall and not box.end:
                         delete_start()
                         box.start = not box.start
-                    elif con == 2:
+                        path = reset_path_etc()
+                        nemu = False
+                        searching = False
+                    elif con == 2 and not box.wall and not box.start:
                         delete_end()
                         box.end = not box.end
+                        path = reset_path_etc()
+                        nemu = False
+                        searching = False
                     elif con == 3:
-                        box.wall = not box.wall
+                        path = reset_path_etc()
+                        nemu = False
+                        searching = False
+                        if not box.start and not box.end:
+                            # box.wall = not box.wall
+                            box.wall = True
                     # messagebox.showinfo("cek aja", f"{con} box {box.x}, {box.y}")
+                    elif con == 4:
+                        path = reset_path_etc()
+                        nemu = False
+                        searching = False
+                        if not box.start and not box.end:
+                            # box.wall = not box.wall
+                            box.wall = False
+
+    for i in range(len(neigh_btn)):
+        if neigh_btn[i].box.collidepoint(pygame.mouse.get_pos()) and i != 4:
+            neigh_btn[i].draw(SCREEN, (169, 169, 169))
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+            if pygame.mouse.get_pressed()[0]:
+                neighbours[i] = not neighbours[i]
+        elif neigh_btn[i].box.collidepoint(pygame.mouse.get_pos()) and i == 4:
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_NO)
+
+    return nemu, path, searching
 
 
-run = True
-while run:
+make_grid()
+
+print(len(neigh_btn))
+
+
+run = [True]
+while run[0]:
+    # print(neighbours)
 
     timer.tick(fps)
 
     if state == 0:
         state = draw_menu()
-        path, nemu = reset_grid()
+        path, nemu, searching, neighbours = reset_grid()
         con = 0
     if state == 1:
-        state, con, searching, queue, cur, nemu = draw_djik(con, searching, queue, cur, nemu)
-        check_collide(con)
+        state, con, searching, queue, cur, nemu, path = draw_djik(con, searching, queue, cur, nemu, path)
+        nemu, path, searching = check_collide(con, nemu, path, searching)
     if state == 2:
-        state, con, searching, stack, cur, nemu = draw_dfs(con, searching, stack, cur, nemu)
-        check_collide(con)
+        state, con, searching, stack, cur, nemu, path = draw_dfs(con, searching, stack, cur, nemu, path)
+        nemu, path, searching = check_collide(con, nemu, path, searching)
     if state == 3:
-        run = False
+        run[0] = False
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
+            run[0] = False
 
     pygame.display.update()
 
